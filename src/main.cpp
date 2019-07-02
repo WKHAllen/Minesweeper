e225f6946a71559ba6980d0747997a15a457f089
wallen <wallen.contractor@rti.org>
2019-07-02 12:05:47 -0400

    Add support for command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include "minesweeper.h"
+#include <string>
+#include <stdlib.h>
 
-int main()
-{
-    Minesweeper game(0, 0, 20);
+int main(int argc, char *argv[]) {
+    int width = 0;
+    int height = 0;
+    int difficulty = -1;
+    for (int i = 1; i < argc; i++) {
+        if (((std::string)argv[i] == "-w" || (std::string)argv[i] == "--width") && i + 1 < argc) {
+            try {
+                width = atoi(argv[++i]);
+            } catch(std::exception) {}
+        }
+        else if (((std::string)argv[i] == "-h" || (std::string)argv[i] == "--height") && i + 1 < argc) {
+            try {
+                height = atoi(argv[++i]);
+            } catch(std::exception) {}
+        }
+        else if (((std::string)argv[i] == "-d" || (std::string)argv[i] == "--difficulty") && i + 1 < argc) {
+            try {
+                difficulty = atoi(argv[++i]);
+            } catch(std::exception) {}
+        }
+    }
+    Minesweeper game(width, height, difficulty);
     game.run();
     return 0;
 }
